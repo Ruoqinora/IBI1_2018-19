554f861cd4981d4a8cb918dd3727454bfc22b923
Nora <Ruoqi.18@intl.zju.edu.cn>
2019-05-11 15:43:03 +0800

    Add Practical 8 files

---
+++ Practical8/find go.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 09:00:39 2019
+
+@author: 19305
+"""
+#import necessary libraries
+import xml.dom.minidom
+import re
+import pandas as pd
+
+filePath=r'D:\IBI\IBI1_2018-19\Practical8';#file path
+fileName='go_obo.xml';
+resName='autophagosome.xlsx'
+file=filePath+'/'+fileName
+res=filePath+'/'+resName
+
+re_immu=re.compile(r'autophagosome')
+#fuction to find childnodes
+def Child(id,resultSet):
+    for t in go:
+        parents=t.getElementsByTagName('is_a')
+        geneid=t.getElementsByTagName('id')[0].childNodes[0].data
+        for parent in parents:
+            if parent.childNodes[0].data==id:
+                resultSet.add(geneid)
+                #do not use resultSet=resultSet|set([geneid])
+                #otherwise you will create a new set instead of modifying the existing one
+                #print(resultSet)
+                Child(geneid,resultSet)
+#create a pandas.Dataframe to store the output
+df=pd.DataFrame(columns=['id','name','defination','childnodes'])
+
+#create the DOM tree
+DOMTree =xml.dom.minidom.parse(file)
+obo=DOMTree.documentElement
+go=obo.getElementsByTagName('term')
+for term in go:
+    defstr=term.getElementsByTagName('defstr')[0].childNodes[0].data
+    #find terms that contain the word 'autophagosome'
+    if re_immu.search(defstr):
+        id=term.getElementsByTagName('id')[0].childNodes[0].data
+        name=term.getElementsByTagName('name')[0].childNodes[0].data
+        resultSet=set()
+        Child(id,resultSet)
+        df=df.append(pd.DataFrame({'id':[id],'name':[name],'defination':[defstr],'childnodes':[len(resultSet)]}))
+        print(id,len(resultSet))
+        #only print these two elements but all in excel file.
+#save to excel
+df.to_excel(res,index=False)
+                
