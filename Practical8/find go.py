9a73846e223db743f7960250c878b41be66e4d4a
Nora <Ruoqi.18@intl.zju.edu.cn>
2019-05-16 23:10:54 +0800

    changes for practical8&9

--- Practical8/find go.py
+++ Practical8/find go.py
@@ -19,19 +19,17 @@ res=filePath+'/'+resName
 
 
 
-re_immu=re.compile(r'autophagosome')
+re_immu=re.compile(r'autophagosome')# replace autophagosome with re_immu for later search
 #fuction to find childnodes
-def Child(id,resultSet):#count childnodes
+def Child(id,resultset):#count childnodes
     for t in go:
-        parents=t.getElementsByTagName('is_a')
+        parents=t.getElementsByTagName('is_a')#find childnodes
         geneid=t.getElementsByTagName('id')[0].childNodes[0].data
         for parent in parents:
             if parent.childNodes[0].data==id:
-                resultSet.add(geneid)
-                #do not use resultSet=resultSet|set([geneid])
-                #otherwise you will create a new set instead of modifying the existing one
+                resultset.add(geneid)
                 #print(resultSet)
-                Child(geneid,resultSet)
+                Child(geneid,resultset)
 #create a pandas.Dataframe to store the output
 df=pd.DataFrame(columns=['id','name','defination','childnodes'])
 
@@ -45,10 +43,10 @@ for term in go:
     if re_immu.search(defstr):
         id=term.getElementsByTagName('id')[0].childNodes[0].data
         name=term.getElementsByTagName('name')[0].childNodes[0].data
-        resultSet=set()
-        Child(id,resultSet)
-        df=df.append(pd.DataFrame({'id':[id],'name':[name],'defination':[defstr],'childnodes':[len(resultSet)]}))
-        print(id,len(resultSet))
+        resultset=set()
+        Child(id,resultset)
+        df=df.append(pd.DataFrame({'id':[id],'name':[name],'defination':[defstr],'childnodes':[len(resultset)]}))
+        print(id,len(resultset))
         #only print these two elements but all in excel file.
 #save to excel
 df.to_excel(res,index=False)
